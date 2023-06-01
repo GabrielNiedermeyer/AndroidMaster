30be946d9f3eb644da6e02aa940de45b15d2e02c
GabrielNIedermeyer <sales.9511@gmail.com>
2023-06-01 20:10:41 +0100

    colors

--- app/src/main/java/com/newdev/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/newdev/androidmaster/MenuActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.newdev.androidmaster.Calculator.CalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +12,18 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
         val btnSaludApp = findViewById<Button>(R.id.btn)
         val btnImcApp = findViewById<Button>(R.id.btnImcApp)
-        btnSaludApp.setOnClickListener { navegateToSaludApp }
+        btnSaludApp.setOnClickListener { navegateToSaludApp() }
+        btnSaludApp.setOnClickListener { navegateToImcApp() }
     }
+
+    private fun navegateToImcApp() {
+        val intent = Intent ( this, CalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
     private fun navegateToSaludApp(){
         val intent = Intent ( this, MenuActivity::class.java)
-
+        startActivity(intent)
 
     }
 }
